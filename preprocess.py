5a39b4486fbf354e8141f06e755fbc2a9639afe6
EIvan21 <edher.diaz21@gmail.com>
2026-02-27 13:11:50 -0600

    Downgraded scikit-learn for compatibility and fixed groupby/encoding logic

--- preprocess.py
+++ preprocess.py
@@ -22,34 +22,29 @@ def calculate_hyper_features(df):
     df['date'] = pd.to_datetime(df['date'])
     df = df.sort_values('date')
     
-    # 1. Base Team Data
     home = df[['fixture_id', 'date', 'league_id', 'home_id', 'goals_home', 'goals_away', 'away_id']].copy()
     away = df[['fixture_id', 'date', 'league_id', 'away_id', 'goals_away', 'goals_home', 'home_id']].copy()
     home.columns = ['fixture_id', 'date', 'league_id', 'team_id', 'gf', 'ga', 'opp_id']
     away.columns = ['fixture_id', 'date', 'league_id', 'team_id', 'gf', 'ga', 'opp_id']
     team_stats = pd.concat([home, away]).sort_values(['team_id', 'date'])
     
-    # 2. Power Score (Elo Proxy)
     group = team_stats.groupby('team_id')
     team_stats['is_win'] = (team_stats['gf'] > team_stats['ga']).astype(int)
     team_stats['power_score'] = group['is_win'].transform(lambda x: x.shift().rolling(10, min_periods=1).mean()).fillna(0.3)
     
-    # 3. Contextual Adjustment
     team_stats = team_stats.merge(team_stats[['fixture_id', 'team_id', 'power_score']], left_on=['fixture_id', 'opp_id'], right_on=['fixture_id', 'team_id'], suffixes=('', '_opp')).drop(columns=['team_id_opp'])
     team_stats['adj_gf'] = team_stats['gf'] * (1 + team_stats['power_score_opp'])
     team_stats['adj_ga'] = team_stats['ga'] * (2 - team_stats['power_score_opp'])
     
-    # 4. Rolling Stats
     group = team_stats.groupby('team_id')
     team_stats['roll_gf'] = group['adj_gf'].transform(lambda x: x.shift().rolling(5, min_periods=1).mean())
     team_stats['roll_ga'] = group['adj_ga'].transform(lambda x: x.shift().rolling(5, min_periods=1).mean())
     team_stats['cs_rate'] = group['ga'].transform(lambda x: (x.shift() == 0).rolling(5, min_periods=1).mean())
     
-    # 5. League Home Advantage Factor
-    league_ha = df.groupby('league_id').apply(lambda x: (x['goals_home'] > x['goals_away']).mean()).to_dict()
+    # Fixed Groupby Warning
+    league_ha = df.groupby('league_id').apply(lambda x: (x['goals_home'] > x['goals_away']).mean(), include_groups=False).to_dict()
     df['league_ha_factor'] = df['league_id'].map(league_ha)
 
-    # 6. Merge stats back
     match_stats = df.copy()
     for suffix in ['home', 'away']:
         tid = 'home_id' if suffix == 'home' else 'away_id'
@@ -57,12 +52,10 @@ def calculate_hyper_features(df):
         match_stats = match_stats.merge(team_stats[cols], left_on=['fixture_id', tid], right_on=['fixture_id', 'team_id'], suffixes=('', f'_{suffix}'))
         match_stats = match_stats.drop(columns=['team_id'])
 
-    # 7. HIGH-SIGNAL TARGET ENCODING (Replaces Label Encoding)
     logging.info("Applying Target Encoding to Venue and Referee...")
-    encoder = TargetEncoder(cols=['venue', 'referee', 'league_id'])
-    # We fit on historical data to avoid leakage
-    match_stats['venue_encoded'] = encoder.fit_transform(match_stats['venue'], match_stats['target'])
-    match_stats['ref_encoded'] = encoder.fit_transform(match_stats['referee'], match_stats['target'])
+    # Robust Encoding fit
+    encoder = TargetEncoder(cols=['venue', 'referee'])
+    match_stats[['venue_encoded', 'ref_encoded']] = encoder.fit_transform(match_stats[['venue', 'referee']], match_stats['target'])
     
     return match_stats.rename(columns={'roll_gf': 'roll_gf_home', 'roll_ga': 'roll_ga_home', 'cs_rate': 'cs_rate_home', 'power_score': 'power_score_home'})
 
